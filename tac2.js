2ac0952e4254e92cf032d1e11cffe6c77b188c4f
William Carty <will.carty@interapthq.com>
2016-09-29 19:15:50 -0400

    css done, html done, logic start

---
+++ tac2.js
@@ -0,0 +1,23 @@
+function checkIfwon{
+
+
+
+}
+$("").on("click");
+
+function startGame{
+    ()
+};
+var clearBoard=$("#reset").click(function(clean){
+
+box1 = $("#box1").text("");
+box2 = $("#box2").text("");
+box3 = $("#box3").text("");
+r2box1= $("#2box1").text("");
+r2box2= $("#2box2").text("");
+r2box3= $("#2box3").text("");
+r3box1= $("#3box1").text("");
+r3box2= $("#3box2").text("");
+r3box3= $("#3box3").text("");
+
+});
